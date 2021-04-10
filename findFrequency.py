917692f93701e466372bdb61fa51a953bebbc4c6
021478963 <76883305+021478963@users.noreply.github.com>
2021-04-10 15:17:13 -0400

    Update findFrequency.py
    added comments
    made it so that the movie title is the first
    item in the list

--- findFrequency.py
+++ findFrequency.py
@@ -1,32 +1,36 @@
 import operator
 
+#takes in list of words
+#returns dictionary of words, frequency
 def findFrequency(wordList):
-	wordFrequency = []
-	wordFrequencyDictionary = {}
+	wordFreq = []
+	wordFreqDict = {}
 
+	#counts words, adds values to dictionary
 	for word in wordList:
-		wordFrequency.append(wordList.count(word))
-		wordFrequencyDictionary.update(zip(wordList, wordFrequency))
-		
-	wordFrequencyDictionary = sorted(wordFrequencyDictionary.items(), key = operator.itemgetter(0))
+		wordFreq.append(wordList.count(word))
+		wordFreqDict.update(zip(wordList, wordFreq))
 	
-	return wordFrequencyDictionary
+	#sorts words alphabetically
+	wordFreqDict = sorted(wordFreqDict.items(), key = operator.itemgetter(0))
+	
+	return wordFreqDict
 
+#takes in list of lists containing string for movie script and movie title
+#returns list consisting of movie title and then (word, frequency)
 def main(wordList, movieTitles):
-	result = {}
+	result = []
 	for scripts, titles in zip(wordList, movieTitles):
-		
-		y = findFrequency(scripts)
-		print(y)
-		print(titles)
+		wordFreqDict = findFrequency(scripts)
+		wordFreqDict.insert(0, titles)
+		result.append(wordFreqDict)
 	return result	
 
-
+#testing
 list1 = "this is a list of words this is a list".split(' ')
 list2 = "this is another list of words".split(' ')
 testTitles = ["Black Panther", "Joker", "Avengers"]
 testScript = [list1, list2]
-z = list(main(testScript, testTitles))
+z = main(testScript, testTitles)
 
-for thing in z:
-	print(list(thing))
+print(z[0])
