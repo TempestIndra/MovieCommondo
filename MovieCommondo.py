3c4680c9f91560875bceb22fdbaa958a561f98f1
Nick Fuangphon <43696183+TempestIndra@users.noreply.github.com>
2021-04-11 04:24:15 -0400

    input file working more clean up URL

--- MovieCommondo.py
+++ MovieCommondo.py
@@ -8,16 +8,24 @@ from bs4 import BeautifulSoup
 def getContent(name):
     URL = "https://imsdb.com/scripts/" + setURL(name) + ".html"
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.content, 'html.parser')
     content = soup.find('pre')
-    # try:
-    # print(content)
-    return content
+    try:
+        if (str(content) == "<pre></pre>"):
+            raise Exception("can't identify link")
+        else:
+            return content
+    except Exception as e:
+        print(str(e) + " "+name)
 
 
 def setURL(name):
-    return name.replace(" ", "-")
+    rname = name.lower()
+    rname = rname.split()
+    for i in range(len(rname)):
+        rname[i] = rname[i].capitalize()
+    rname = convertListToString(rname)
+    return rname.replace(" ", "-")[:-1]
 
 
 def cleanUpData(data):
@@ -37,10 +45,11 @@ def cleanUpData(data):
     stringData = stringData.replace("\"", " ")
     stringData = stringData.replace("!", " ")
     stringData = stringData.replace(";", " ")
+    stringData = stringData.replace("&", " ")
+    stringData = stringData.replace("#", " ")
+    for i in range(0, 10):
+        stringData = stringData.replace(str(i), " ")
     stringData = stringData.split()
-    for i in stringData:
-        if(i.isdigit() == True):
-            stringData.remove(i)
     return convertListToString(stringData)
 
 
@@ -53,23 +62,36 @@ def convertListToString(listData):
 
 def main(argv):
     inputfile = ''
+    inputData = []
+    scriptData = []
     outputfile = ''
     try:
         opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
+
         print("whenGet erroe sections")
         sys.exit(2)
+
     for opt, arg in opts:
         if opt == '-h':
             print("help section")
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
+            try:
+                with open(arg, "r") as f:
+                    for line in f:
+                        sLine = line.strip()
+                        inputData.append(setURL(sLine))
+            except IOError:
+                print("File not ccessible")
+
         elif opt in ("-o", "--ofile"):
             outputfile = arg
-
-    data = cleanUpData(getContent(sys.argv[1]))
-    print(data)
+        else:
+            inputData.append(sys.argv[1])
+    for i in range(len(inputData)):
+        scriptData.append(cleanUpData(getContent(inputData[i])))
 
 
 if __name__ == "__main__":
